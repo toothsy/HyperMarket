fe03b7273191c55aa97872e6f5582327f597cef9
Karan Sanal <karansanal23@gmail.com>
2022-03-03 17:35:34 +0530

    Added Interface folder

---
+++ HyperMarket/Data/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using HyperMarket.DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperMarket.DB.Interfaces
+{
+    public interface IUserService
+    {
+        List<User> GetUsers();
+        User GetUserById(int id);
+        void SaveUser(User user);
+        void DeleteUser(int id);
+    }
+}
---
+++ HyperMarket/Data/Interfaces/UserService.cs
@@ -0,0 +1,43 @@
+﻿using HyperMarket.Data;
+using HyperMarket.DB.Models;
+
+namespace HyperMarket.DB.Interfaces
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public List<User> GetUsers()
+        {
+            return _dbContext.Users.ToList();
+        }
+
+        public User GetUserById(int id)
+        {
+            var user = _dbContext.Users.FirstOrDefault(x => x.UserId == id);
+            return user;
+        }
+
+        public void SaveUser(User user)
+        {
+            if (user.UserId == 0) _dbContext.Users.Add(user);
+            else _dbContext.Users.Update(user);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            var customer = _dbContext.Users.FirstOrDefault(x => x.UserId == id);
+            if (customer != null)
+            {
+                _dbContext.Users.Remove(customer);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+}
