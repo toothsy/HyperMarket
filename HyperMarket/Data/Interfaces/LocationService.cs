19bb88fdb88c00d92e6fbddbf20939a904bbbe1c
supra <supra@LAPTOP-6LH5G87J>
2022-03-09 19:52:11 +0530

    Updated

---
+++ HyperMarket/Data/Interfaces/LocationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HyperMarket.DB.Models;
+
+
+using HyperMarket.Data;
+using System.Net.Http.Json;
+
+namespace HyperMarket.Data.Interfaces
+{
+    public class LocationService : ILocationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LocationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<Location> GetLocationById(int id)
+        {
+            var locat = await _httpClient.GetFromJsonAsync<Location>($"/api/location/{id}");
+            return locat;
+        }
+    }
+}
---
+++ HyperMarket/Data/Interfaces/PaymentService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HyperMarket.DB.Models;
+
+
+using HyperMarket.Data;
+using System.Net.Http.Json;
+
+namespace HyperMarket.Data.Interfaces
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PaymentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<Payment> GetPaymentById(int id)
+        {
+            var pay = await _httpClient.GetFromJsonAsync<Payment>($"/api/payment/{id}");
+            return pay;
+        }
+    }
+}
